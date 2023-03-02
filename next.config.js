15bf970f9df30fdeadd34a63eeefc06f0cd81a84
Brian <nbmbestre91@gmail.com>
2023-03-02 23:07:49 +0800

    Modularize Material imports

--- next.config.js
+++ next.config.js
@@ -4,6 +4,22 @@ const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
+  experimental: {
+    modularizeImports: {
+      '@mui/material': {
+        transform: '@mui/material/{{member}}'
+      },
+      '@mui/joy': {
+        transform: '@mui/joy/{{member}}'
+      },
+      '@mui/lab': {
+        transform: '@mui/lab/{{member}}'
+      },
+      '@mui/icons-material/?(((\\w*)?/?)*)': {
+        transform: '@mui/icons-material/{{ matches.[1] }}/{{member}}'
+      }
+    }
+  }
 }
 
 module.exports = nextConfig
